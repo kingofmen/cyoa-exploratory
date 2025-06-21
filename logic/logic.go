adbf6490d0cf702b7d1f58d8ac559cfddd2f73da
kingofmen <rolf.andreassen@gmail.com>
2025-06-21 14:18:58 -0700

    Predicate logic library.

---
+++ logic/logic.go
@@ -0,0 +1,249 @@
+// Package logic provides boolean logic on predicates defined by protobufs.
+package logic
+
+import (
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
+
+	lpb "github.com/kingofmen/cyoa-exploratory/logic/proto"
+)
+
+const (
+	kScopeSeparator = "."
+)
+
+type Lookup interface {
+	GetInt(key string) (int32, error)
+	GetStr(key string) (string, error)
+	GetStrArr(key string) ([]string, error)
+	GetScope(key string) Lookup
+	SetScope(key string, scope Lookup)
+	ListScopes() []string
+}
+
+// Scoper provides a default in-memory scope-management object
+// which implementors of the Lookup interface can embed if they
+// don't want to roll their own.
+type Scoper struct {
+	scopes map[string]Lookup
+}
+
+// GetScope returns the given lookup scope.
+func (sc *Scoper) GetScope(key string) Lookup {
+	if sc == nil {
+		return nil
+	}
+	return sc.scopes[key]
+}
+
+// SetScope sets the key to point to the scope.
+func (sc *Scoper) SetScope(key string, scope Lookup) {
+	if sc == nil {
+		return
+	}
+	if sc.scopes == nil {
+		sc.scopes = make(map[string]Lookup)
+	}
+	sc.scopes[key] = scope
+}
+
+// ListScopes returns the keys of any existing scopes.
+func (sc *Scoper) ListScopes() []string {
+	if sc == nil {
+		return nil
+	}
+	ret := make([]string, 0, len(sc.scopes))
+	for key := range sc.scopes {
+		ret = append(ret, key)
+	}
+	sort.Strings(ret)
+	return ret
+}
+
+// evalCombination returns true if the logical expression is true.
+func evalCombination(comb *lpb.Combine, lookup Lookup) (bool, error) {
+	switch comb.GetOperation() {
+	case lpb.Combine_IF_ALL:
+		for _, p := range comb.GetOperands() {
+			v, err := Eval(p, lookup)
+			if err != nil {
+				return false, err
+			}
+			if !v {
+				return false, nil
+			}
+		}
+		return true, nil
+	case lpb.Combine_IF_ANY:
+		for _, p := range comb.GetOperands() {
+			v, err := Eval(p, lookup)
+			if err != nil {
+				return false, err
+			}
+			if v {
+				return true, nil
+			}
+		}
+		return false, nil
+	case lpb.Combine_IF_NONE:
+		for _, p := range comb.GetOperands() {
+			v, err := Eval(p, lookup)
+			if err != nil {
+				return false, err
+			}
+			if v {
+				return false, nil
+			}
+		}
+		return true, nil
+	}
+	return false, nil
+}
+
+// getInt returns an integer either because key is a literal,
+// or from the lookup table.
+func getInt(key string, lookup Lookup) (int32, error) {
+	if val, err := strconv.Atoi(key); err == nil {
+		return int32(val), nil
+	}
+	scope, skey, has := strings.Cut(key, kScopeSeparator)
+	if has {
+		slookup := lookup.GetScope(scope)
+		if slookup == nil {
+			return 0, fmt.Errorf("invalid scope lookup %q from integer key %q", scope, key)
+		}
+		return getInt(skey, slookup)
+	}
+
+	return lookup.GetInt(key)
+}
+
+// getStr returns a string either because key is a literal,
+// or from the lookup table.
+func getStr(key string, lookup Lookup) (string, error) {
+	if len(key) > 0 && key[0] == byte('\'') {
+		return key[1:], nil
+	}
+	scope, skey, has := strings.Cut(key, kScopeSeparator)
+	if has {
+		slookup := lookup.GetScope(scope)
+		if slookup == nil {
+			return "", fmt.Errorf("invalid scope lookup %q from string key %q", scope, key)
+		}
+		return getStr(skey, slookup)
+	}
+	return lookup.GetStr(key)
+}
+
+// getStrArr returns a string array, either from the lookup table
+// or from parsing an array literal.
+func getStrArr(key string, lookup Lookup) ([]string, error) {
+	if l, ok := strings.CutSuffix(key, "]"); ok {
+		if literal, ok := strings.CutPrefix(l, "["); ok {
+			entries := strings.Split(literal, ",")
+			for idx, entry := range entries {
+				val, err := getStr(strings.Trim(entry, " "), lookup)
+				if err != nil {
+					return nil, fmt.Errorf("error constructing array entry %q: %w", entry, err)
+				}
+				entries[idx] = val
+			}
+			return entries, nil
+		}
+	}
+	scope, skey, has := strings.Cut(key, kScopeSeparator)
+	if has {
+		slookup := lookup.GetScope(scope)
+		if slookup == nil {
+			return nil, fmt.Errorf("invalid scope lookup %q from string array key %q", scope, key)
+		}
+		return getStrArr(skey, slookup)
+	}
+	return lookup.GetStrArr(key)
+}
+
+// evalIntComparison returns the truth-value of the integer predicate.
+func evalIntComparison(comp *lpb.Compare, lookup Lookup) (bool, error) {
+	one, err := getInt(comp.GetKeyOne(), lookup)
+	if err != nil {
+		return false, err
+	}
+	two, err := getInt(comp.GetKeyTwo(), lookup)
+	if err != nil {
+		return false, err
+	}
+	switch comp.GetOperation() {
+	case lpb.Compare_CMP_GT:
+		return one > two, nil
+	case lpb.Compare_CMP_LT:
+		return one < two, nil
+	case lpb.Compare_CMP_EQ:
+		return one == two, nil
+	case lpb.Compare_CMP_GTE:
+		return one >= two, nil
+	case lpb.Compare_CMP_LTE:
+		return one <= two, nil
+	case lpb.Compare_CMP_NEQ:
+		return one != two, nil
+	}
+	return false, fmt.Errorf("cannot evaluate unknown (int) operator %d %v %d", one, comp.GetOperation(), two)
+}
+
+// evalStrComparison returns the truth-value of the string predicate.
+func evalStrComparison(comp *lpb.Compare, lookup Lookup) (bool, error) {
+	one, err := getStr(comp.GetKeyOne(), lookup)
+	if err != nil {
+		return false, err
+	}
+	op := comp.GetOperation()
+	key := comp.GetKeyTwo()
+	if op == lpb.Compare_CMP_STRIN {
+		return evalStrIn(one, key, lookup)
+	}
+	two, err := getStr(key, lookup)
+	if err != nil {
+		return false, err
+	}
+	switch op {
+	case lpb.Compare_CMP_STREQ:
+		return one == two, nil
+	}
+	return false, fmt.Errorf("cannot evaluate unknown (string) operator %q %v %q", one, op, two)
+}
+
+// evalStrIn returns whether key is in the array.
+func evalStrIn(key, arrKey string, lookup Lookup) (bool, error) {
+	arr, err := getStrArr(arrKey, lookup)
+	if err != nil {
+		return false, err
+	}
+	for _, val := range arr {
+		if key == val {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+func evalComparison(comp *lpb.Compare, lookup Lookup) (bool, error) {
+	op := comp.GetOperation()
+	// Check for string operations.
+	if op == lpb.Compare_CMP_STREQ || op == lpb.Compare_CMP_STRIN {
+		return evalStrComparison(comp, lookup)
+	}
+
+	return evalIntComparison(comp, lookup)
+}
+
+// Eval returns the truth value of the predicate.
+func Eval(pred *lpb.Predicate, lookup Lookup) (bool, error) {
+	if comb := pred.GetComb(); comb != nil {
+		return evalCombination(comb, lookup)
+	}
+	if comp := pred.GetComp(); comp != nil {
+		return evalComparison(comp, lookup)
+	}
+	return false, nil
+}
---
+++ logic/logic_test.go
@@ -0,0 +1,761 @@
+package logic
+
+import (
+	"testing"
+
+	"google.golang.org/protobuf/proto"
+
+	lpb "github.com/kingofmen/cyoa-exploratory/logic/proto"
+)
+
+func TestBasics(t *testing.T) {
+	defaults := NewTestLookup().
+		WithInt("one", 1).
+		WithInt("en", 1).
+		WithInt("two", 2).
+		WithStr("string1", "yohoho").
+		WithStr("string2", "yohoho").
+		WithStr("string3", "bwahaha").
+		WithStrArr("strarr1", []string{"yohoho", "bwahaha"})
+
+	cases := []struct {
+		desc   string
+		pred   *lpb.Predicate
+		lookup Lookup
+		want   bool
+	}{
+		{
+			desc: "Greater than (false)",
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comp{
+					Comp: &lpb.Compare{
+						KeyOne:    proto.String("one"),
+						KeyTwo:    proto.String("two"),
+						Operation: lpb.Compare_CMP_GT.Enum(),
+					},
+				},
+			},
+			lookup: defaults,
+			want:   false,
+		},
+		{
+			desc: "Greater than (true)",
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comp{
+					Comp: &lpb.Compare{
+						KeyOne:    proto.String("two"),
+						KeyTwo:    proto.String("one"),
+						Operation: lpb.Compare_CMP_GT.Enum(),
+					},
+				},
+			},
+			lookup: defaults,
+			want:   true,
+		},
+		{
+			desc: "Less than (false)",
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comp{
+					Comp: &lpb.Compare{
+						KeyOne:    proto.String("two"),
+						KeyTwo:    proto.String("one"),
+						Operation: lpb.Compare_CMP_LT.Enum(),
+					},
+				},
+			},
+			lookup: defaults,
+			want:   false,
+		},
+		{
+			desc: "Less than (true)",
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comp{
+					Comp: &lpb.Compare{
+						KeyOne:    proto.String("one"),
+						KeyTwo:    proto.String("two"),
+						Operation: lpb.Compare_CMP_LT.Enum(),
+					},
+				},
+			},
+			lookup: defaults,
+			want:   true,
+		},
+		{
+			desc: "Equal (false)",
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comp{
+					Comp: &lpb.Compare{
+						KeyOne:    proto.String("two"),
+						KeyTwo:    proto.String("one"),
+						Operation: lpb.Compare_CMP_EQ.Enum(),
+					},
+				},
+			},
+			lookup: defaults,
+			want:   false,
+		},
+		{
+			desc: "Equal (true)",
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comp{
+					Comp: &lpb.Compare{
+						KeyOne:    proto.String("one"),
+						KeyTwo:    proto.String("en"),
+						Operation: lpb.Compare_CMP_EQ.Enum(),
+					},
+				},
+			},
+			lookup: defaults,
+			want:   true,
+		},
+		{
+			desc: "Greater than or equal (false)",
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comp{
+					Comp: &lpb.Compare{
+						KeyOne:    proto.String("one"),
+						KeyTwo:    proto.String("two"),
+						Operation: lpb.Compare_CMP_GTE.Enum(),
+					},
+				},
+			},
+			lookup: defaults,
+			want:   false,
+		},
+		{
+			desc: "Greater than or equal (true, greater)",
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comp{
+					Comp: &lpb.Compare{
+						KeyOne:    proto.String("two"),
+						KeyTwo:    proto.String("one"),
+						Operation: lpb.Compare_CMP_GTE.Enum(),
+					},
+				},
+			},
+			lookup: defaults,
+			want:   true,
+		},
+		{
+			desc: "Greater than or equal (true, equal)",
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comp{
+					Comp: &lpb.Compare{
+						KeyOne:    proto.String("en"),
+						KeyTwo:    proto.String("one"),
+						Operation: lpb.Compare_CMP_GTE.Enum(),
+					},
+				},
+			},
+			lookup: defaults,
+			want:   true,
+		},
+		{
+			desc: "Less than or equal (false)",
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comp{
+					Comp: &lpb.Compare{
+						KeyOne:    proto.String("two"),
+						KeyTwo:    proto.String("one"),
+						Operation: lpb.Compare_CMP_LTE.Enum(),
+					},
+				},
+			},
+			lookup: defaults,
+			want:   false,
+		},
+		{
+			desc: "Less than or equal (true, less than)",
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comp{
+					Comp: &lpb.Compare{
+						KeyOne:    proto.String("one"),
+						KeyTwo:    proto.String("two"),
+						Operation: lpb.Compare_CMP_LTE.Enum(),
+					},
+				},
+			},
+			lookup: defaults,
+			want:   true,
+		},
+		{
+			desc: "Less than or equal (true, equal)",
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comp{
+					Comp: &lpb.Compare{
+						KeyOne:    proto.String("one"),
+						KeyTwo:    proto.String("en"),
+						Operation: lpb.Compare_CMP_LTE.Enum(),
+					},
+				},
+			},
+			lookup: defaults,
+			want:   true,
+		},
+		{
+			desc: "Not equal (false)",
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comp{
+					Comp: &lpb.Compare{
+						KeyOne:    proto.String("two"),
+						KeyTwo:    proto.String("one"),
+						Operation: lpb.Compare_CMP_NEQ.Enum(),
+					},
+				},
+			},
+			lookup: defaults,
+			want:   true,
+		},
+		{
+			desc: "Not equal (true)",
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comp{
+					Comp: &lpb.Compare{
+						KeyOne:    proto.String("one"),
+						KeyTwo:    proto.String("en"),
+						Operation: lpb.Compare_CMP_NEQ.Enum(),
+					},
+				},
+			},
+			lookup: defaults,
+			want:   false,
+		},
+		{
+			desc: "String equals (false)",
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comp{
+					Comp: &lpb.Compare{
+						KeyOne:    proto.String("string1"),
+						KeyTwo:    proto.String("string3"),
+						Operation: lpb.Compare_CMP_STREQ.Enum(),
+					},
+				},
+			},
+			lookup: defaults,
+			want:   false,
+		},
+		{
+			desc: "String equals (true)",
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comp{
+					Comp: &lpb.Compare{
+						KeyOne:    proto.String("string1"),
+						KeyTwo:    proto.String("string2"),
+						Operation: lpb.Compare_CMP_STREQ.Enum(),
+					},
+				},
+			},
+			lookup: defaults,
+			want:   true,
+		},
+		{
+			desc: "Integer literals",
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comp{
+					Comp: &lpb.Compare{
+						KeyOne:    proto.String("1"),
+						KeyTwo:    proto.String("2"),
+						Operation: lpb.Compare_CMP_GT.Enum(),
+					},
+				},
+			},
+			lookup: defaults,
+			want:   false,
+		},
+		{
+			desc: "Integer literal mixed with variable",
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comp{
+					Comp: &lpb.Compare{
+						KeyOne:    proto.String("two"),
+						KeyTwo:    proto.String("1"),
+						Operation: lpb.Compare_CMP_GT.Enum(),
+					},
+				},
+			},
+			lookup: defaults,
+			want:   true,
+		},
+		{
+			desc: "String literals",
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comp{
+					Comp: &lpb.Compare{
+						KeyOne:    proto.String("'literal"),
+						KeyTwo:    proto.String("'another literal"),
+						Operation: lpb.Compare_CMP_STREQ.Enum(),
+					},
+				},
+			},
+			lookup: defaults,
+			want:   false,
+		},
+		{
+			desc: "String literal mixed with lookup",
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comp{
+					Comp: &lpb.Compare{
+						KeyOne:    proto.String("'yohoho"),
+						KeyTwo:    proto.String("string1"),
+						Operation: lpb.Compare_CMP_STREQ.Enum(),
+					},
+				},
+			},
+			lookup: defaults,
+			want:   true,
+		},
+		{
+			desc: "String in array (false)",
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comp{
+					Comp: &lpb.Compare{
+						KeyOne:    proto.String("'banana"),
+						KeyTwo:    proto.String("strarr1"),
+						Operation: lpb.Compare_CMP_STRIN.Enum(),
+					},
+				},
+			},
+			lookup: defaults,
+			want:   false,
+		},
+		{
+			desc: "String in array (true)",
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comp{
+					Comp: &lpb.Compare{
+						KeyOne:    proto.String("string1"),
+						KeyTwo:    proto.String("strarr1"),
+						Operation: lpb.Compare_CMP_STRIN.Enum(),
+					},
+				},
+			},
+			lookup: defaults,
+			want:   true,
+		},
+		{
+			desc: "String in array literal (false)",
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comp{
+					Comp: &lpb.Compare{
+						KeyOne:    proto.String("'carrot"),
+						KeyTwo:    proto.String("['apple, 'banana, string1]"),
+						Operation: lpb.Compare_CMP_STRIN.Enum(),
+					},
+				},
+			},
+			lookup: defaults,
+			want:   false,
+		},
+		{
+			desc: "String in array literal (true)",
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comp{
+					Comp: &lpb.Compare{
+						KeyOne:    proto.String("'yohoho"),
+						KeyTwo:    proto.String("['apple, 'banana, string1]"),
+						Operation: lpb.Compare_CMP_STRIN.Enum(),
+					},
+				},
+			},
+			lookup: defaults,
+			want:   true,
+		},
+	}
+
+	for _, cc := range cases {
+		t.Run(cc.desc, func(t *testing.T) {
+			got, err := Eval(cc.pred, cc.lookup)
+			if err != nil {
+				t.Errorf("%s: Eval() => %v, want nil", cc.desc, err)
+			}
+			if got != cc.want {
+				t.Errorf("%s: Eval() => %v, want %v", cc.desc, got, cc.want)
+			}
+		})
+	}
+}
+
+func TestCombinations(t *testing.T) {
+	defaults := NewTestLookup().
+		WithInt("one", 1).
+		WithInt("en", 1).
+		WithInt("two", 2)
+
+	cases := []struct {
+		desc   string
+		pred   *lpb.Predicate
+		lookup Lookup
+		want   bool
+	}{
+		{
+			desc: "All (true)",
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comb{
+					Comb: &lpb.Combine{
+						Operands: []*lpb.Predicate{
+							&lpb.Predicate{
+								Test: &lpb.Predicate_Comp{
+									Comp: &lpb.Compare{
+										KeyOne:    proto.String("one"),
+										KeyTwo:    proto.String("two"),
+										Operation: lpb.Compare_CMP_LT.Enum(),
+									},
+								},
+							},
+							&lpb.Predicate{
+								Test: &lpb.Predicate_Comp{
+									Comp: &lpb.Compare{
+										KeyOne:    proto.String("two"),
+										KeyTwo:    proto.String("one"),
+										Operation: lpb.Compare_CMP_GT.Enum(),
+									},
+								},
+							},
+							&lpb.Predicate{
+								Test: &lpb.Predicate_Comp{
+									Comp: &lpb.Compare{
+										KeyOne:    proto.String("one"),
+										KeyTwo:    proto.String("en"),
+										Operation: lpb.Compare_CMP_EQ.Enum(),
+									},
+								},
+							},
+						},
+						Operation: lpb.Combine_IF_ALL.Enum(),
+					},
+				},
+			},
+			lookup: defaults,
+			want:   true,
+		},
+		{
+			desc: "All (false)",
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comb{
+					Comb: &lpb.Combine{
+						Operands: []*lpb.Predicate{
+							&lpb.Predicate{
+								Test: &lpb.Predicate_Comp{
+									Comp: &lpb.Compare{
+										KeyOne:    proto.String("one"),
+										KeyTwo:    proto.String("two"),
+										Operation: lpb.Compare_CMP_LT.Enum(),
+									},
+								},
+							},
+							&lpb.Predicate{
+								Test: &lpb.Predicate_Comp{
+									Comp: &lpb.Compare{
+										KeyOne:    proto.String("two"),
+										KeyTwo:    proto.String("one"),
+										Operation: lpb.Compare_CMP_GT.Enum(),
+									},
+								},
+							},
+							&lpb.Predicate{
+								Test: &lpb.Predicate_Comp{
+									Comp: &lpb.Compare{
+										KeyOne:    proto.String("one"),
+										KeyTwo:    proto.String("two"),
+										Operation: lpb.Compare_CMP_EQ.Enum(),
+									},
+								},
+							},
+						},
+						Operation: lpb.Combine_IF_ALL.Enum(),
+					},
+				},
+			},
+			lookup: defaults,
+			want:   false,
+		},
+		{
+			desc: "Any (true)",
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comb{
+					Comb: &lpb.Combine{
+						Operands: []*lpb.Predicate{
+							&lpb.Predicate{
+								Test: &lpb.Predicate_Comp{
+									Comp: &lpb.Compare{
+										KeyOne:    proto.String("one"),
+										KeyTwo:    proto.String("two"),
+										Operation: lpb.Compare_CMP_GT.Enum(),
+									},
+								},
+							},
+							&lpb.Predicate{
+								Test: &lpb.Predicate_Comp{
+									Comp: &lpb.Compare{
+										KeyOne:    proto.String("two"),
+										KeyTwo:    proto.String("one"),
+										Operation: lpb.Compare_CMP_LT.Enum(),
+									},
+								},
+							},
+							&lpb.Predicate{
+								Test: &lpb.Predicate_Comp{
+									Comp: &lpb.Compare{
+										KeyOne:    proto.String("one"),
+										KeyTwo:    proto.String("two"),
+										Operation: lpb.Compare_CMP_NEQ.Enum(),
+									},
+								},
+							},
+						},
+						Operation: lpb.Combine_IF_ANY.Enum(),
+					},
+				},
+			},
+			lookup: defaults,
+			want:   true,
+		},
+		{
+			desc: "Any (false)",
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comb{
+					Comb: &lpb.Combine{
+						Operands: []*lpb.Predicate{
+							&lpb.Predicate{
+								Test: &lpb.Predicate_Comp{
+									Comp: &lpb.Compare{
+										KeyOne:    proto.String("one"),
+										KeyTwo:    proto.String("two"),
+										Operation: lpb.Compare_CMP_GTE.Enum(),
+									},
+								},
+							},
+							&lpb.Predicate{
+								Test: &lpb.Predicate_Comp{
+									Comp: &lpb.Compare{
+										KeyOne:    proto.String("two"),
+										KeyTwo:    proto.String("one"),
+										Operation: lpb.Compare_CMP_LTE.Enum(),
+									},
+								},
+							},
+							&lpb.Predicate{
+								Test: &lpb.Predicate_Comp{
+									Comp: &lpb.Compare{
+										KeyOne:    proto.String("one"),
+										KeyTwo:    proto.String("two"),
+										Operation: lpb.Compare_CMP_EQ.Enum(),
+									},
+								},
+							},
+						},
+						Operation: lpb.Combine_IF_ANY.Enum(),
+					},
+				},
+			},
+			lookup: defaults,
+			want:   false,
+		},
+		{
+			desc: "None (false)",
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comb{
+					Comb: &lpb.Combine{
+						Operands: []*lpb.Predicate{
+							&lpb.Predicate{
+								Test: &lpb.Predicate_Comp{
+									Comp: &lpb.Compare{
+										KeyOne:    proto.String("one"),
+										KeyTwo:    proto.String("two"),
+										Operation: lpb.Compare_CMP_GT.Enum(),
+									},
+								},
+							},
+							&lpb.Predicate{
+								Test: &lpb.Predicate_Comp{
+									Comp: &lpb.Compare{
+										KeyOne:    proto.String("two"),
+										KeyTwo:    proto.String("one"),
+										Operation: lpb.Compare_CMP_LT.Enum(),
+									},
+								},
+							},
+							&lpb.Predicate{
+								Test: &lpb.Predicate_Comp{
+									Comp: &lpb.Compare{
+										KeyOne:    proto.String("one"),
+										KeyTwo:    proto.String("two"),
+										Operation: lpb.Compare_CMP_NEQ.Enum(),
+									},
+								},
+							},
+						},
+						Operation: lpb.Combine_IF_NONE.Enum(),
+					},
+				},
+			},
+			lookup: defaults,
+			want:   false,
+		},
+		{
+			desc: "None (true)",
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comb{
+					Comb: &lpb.Combine{
+						Operands: []*lpb.Predicate{
+							&lpb.Predicate{
+								Test: &lpb.Predicate_Comp{
+									Comp: &lpb.Compare{
+										KeyOne:    proto.String("one"),
+										KeyTwo:    proto.String("two"),
+										Operation: lpb.Compare_CMP_GTE.Enum(),
+									},
+								},
+							},
+							&lpb.Predicate{
+								Test: &lpb.Predicate_Comp{
+									Comp: &lpb.Compare{
+										KeyOne:    proto.String("two"),
+										KeyTwo:    proto.String("one"),
+										Operation: lpb.Compare_CMP_LTE.Enum(),
+									},
+								},
+							},
+							&lpb.Predicate{
+								Test: &lpb.Predicate_Comp{
+									Comp: &lpb.Compare{
+										KeyOne:    proto.String("one"),
+										KeyTwo:    proto.String("two"),
+										Operation: lpb.Compare_CMP_EQ.Enum(),
+									},
+								},
+							},
+						},
+						Operation: lpb.Combine_IF_NONE.Enum(),
+					},
+				},
+			},
+			lookup: defaults,
+			want:   true,
+		},
+	}
+
+	for _, cc := range cases {
+		t.Run(cc.desc, func(t *testing.T) {
+			got, err := Eval(cc.pred, cc.lookup)
+			if err != nil {
+				t.Errorf("%s: Eval() => %v, want nil", cc.desc, err)
+			}
+			if got != cc.want {
+				t.Errorf("%s: Eval() => %v, want %v", cc.desc, got, cc.want)
+			}
+		})
+	}
+}
+
+func TestScopes(t *testing.T) {
+	cases := []struct {
+		desc   string
+		base   *TestLookup
+		scopes map[string]*TestLookup
+		pred   *lpb.Predicate
+		want   bool
+	}{
+		{
+			desc: "Compare base and scope",
+			base: NewTestLookup().WithStr("something", "abc"),
+			scopes: map[string]*TestLookup{
+				"scope": NewTestLookup().WithStr("another", "abc"),
+			},
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comp{
+					Comp: &lpb.Compare{
+						KeyOne:    proto.String("something"),
+						KeyTwo:    proto.String("scope.another"),
+						Operation: lpb.Compare_CMP_STREQ.Enum(),
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			desc: "Base string in scope array",
+			base: NewTestLookup().WithStr("something", "abc"),
+			scopes: map[string]*TestLookup{
+				"scope": NewTestLookup().WithStrArr("another", []string{"abc", "def"}),
+			},
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comp{
+					Comp: &lpb.Compare{
+						KeyOne:    proto.String("something"),
+						KeyTwo:    proto.String("scope.another"),
+						Operation: lpb.Compare_CMP_STRIN.Enum(),
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			desc: "String literal in scope array",
+			base: NewTestLookup(),
+			scopes: map[string]*TestLookup{
+				"scope": NewTestLookup().WithStrArr("another", []string{"abc", "def"}),
+			},
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comp{
+					Comp: &lpb.Compare{
+						KeyOne:    proto.String("'def"),
+						KeyTwo:    proto.String("scope.another"),
+						Operation: lpb.Compare_CMP_STRIN.Enum(),
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			desc: "String from one scope in array from another",
+			base: NewTestLookup(),
+			scopes: map[string]*TestLookup{
+				"tele":  NewTestLookup().WithStr("foo", "abc"),
+				"scope": NewTestLookup().WithStrArr("another", []string{"abc", "def"}),
+			},
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comp{
+					Comp: &lpb.Compare{
+						KeyOne:    proto.String("tele.foo"),
+						KeyTwo:    proto.String("scope.another"),
+						Operation: lpb.Compare_CMP_STRIN.Enum(),
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			desc: "String from scope in array literal",
+			base: NewTestLookup(),
+			scopes: map[string]*TestLookup{
+				"tele": NewTestLookup().WithStr("foo", "abc"),
+			},
+			pred: &lpb.Predicate{
+				Test: &lpb.Predicate_Comp{
+					Comp: &lpb.Compare{
+						KeyOne:    proto.String("tele.foo"),
+						KeyTwo:    proto.String("['abc, 'def]"),
+						Operation: lpb.Compare_CMP_STRIN.Enum(),
+					},
+				},
+			},
+			want: true,
+		},
+	}
+
+	for _, cc := range cases {
+		t.Run(cc.desc, func(t *testing.T) {
+			for key, scope := range cc.scopes {
+				cc.base.SetScope(key, scope)
+			}
+			got, err := Eval(cc.pred, cc.base)
+			if err != nil {
+				t.Fatalf("%s: Eval() => %v, want nil", cc.desc, err)
+			}
+			if got != cc.want {
+				t.Errorf("%s: Eval() => %v, want %v", cc.desc, got, cc.want)
+			}
+		})
+	}
+}
