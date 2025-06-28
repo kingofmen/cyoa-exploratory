ae5d57e27b44d818afef03fc8959a5ba5030216d
kingofmen <rolf.andreassen@gmail.com>
2025-06-28 16:40:48 -0700

    Narrator stub package.

---
+++ narrate/narrate.go
@@ -0,0 +1,25 @@
+// Package narrate defines an interface for creating story text
+// from game events.
+package narrate
+
+import (
+	"context"
+
+	storypb "github.com/kingofmen/cyoa-exploratory/story/proto"
+)
+
+type Narrator interface {
+	Event(context.Context, *storypb.GameEvent) (string, error)
+}
+
+// Default is a very silly placeholder narrator which merely
+// echoes back the title of the provided action.
+type Default struct{}
+
+func (d *Default) Event(_ context.Context, event *storypb.GameEvent) (string, error) {
+	return "placeholder", nil
+}
+
+func DefaultNarrator() Narrator {
+	return &Default{}
+}
